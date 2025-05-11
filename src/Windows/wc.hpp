618715397eee8a4d486d47553d991ff14e6ecfa4
Blessing <babumere04@gmail.com>
2025-05-11 18:29:20 -0400

    Update wc.hpp

--- src/Windows/wc.hpp
+++ src/Windows/wc.hpp
@@ -3,11 +3,32 @@
 void Shell::Word_Count(const string& file) {
     size_t lines = 1, words = 0;
 
-    const string& text = Shell::Read_File(file);
-    if (text.empty()) return;
+    if (file.empty()) {
+        fprintf(stderr, "No files were given\n");
+        cout << "wc <files>" << endl;
+        return;
+    }
+    HANDLE hFile = CreateFileA(file.c_str(),           // File name
+                               GENERIC_READ,           // Read access
+                               FILE_SHARE_READ,        // Allow other reads
+                               NULL,                   // Default security
+                               OPEN_EXISTING,          // if exists
+                               FILE_ATTRIBUTE_NORMAL,  // Normal file
+                               NULL);                  // No template
 
-    // number of characters is how ever long the text is
-    const size_t chars = text.length();
+    if (hFile == INVALID_HANDLE_VALUE) {
+        fprintf(stderr, "Failed to open '%s'\n", file.c_str());
+        return;
+    }
+
+    char buffer[BUFFER_SIZE];
+    unsigned long bytes = 0;  // readfile doesnt allow size_t??
+    string text;
+
+    while (ReadFile(hFile, buffer, sizeof(buffer), &bytes, NULL) && bytes > 0)
+        text.append(buffer, bytes);
+
+    CloseHandle(hFile);
 
     for (const char& c : text)
         if (c == '\n') lines++;
@@ -17,7 +38,7 @@ void Shell::Word_Count(const string& file) {
     while (ss >> word) words++;
 
     cout << file << endl;
-    cout << "Characters: " << chars << endl;
+    cout << "Characters: " << text.length() << endl;
     cout << "Words: " << words << endl;
     cout << "Lines: " << lines << endl;
 }
