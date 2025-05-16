e62b9581cd4cd47f9d48bf92e9ada6506c1dfa32
Blessing <babumere04@gmail.com>
2025-05-16 07:38:25 -0400

    Update rmdir.hpp

--- src/Unix/rmdir.hpp
+++ src/Unix/rmdir.hpp
@@ -9,6 +9,6 @@ void Shell::Remove_Directories(const vector<string> &directories)
     }
 
     for (const string &dir : directories)
-        if (rmdir(dir.c_str()) != 0)
+        if (syscall(SYS_rmdir, dir.c_str()) != 0)
             perror(("Failed to remove directory " + dir).c_str());
 }
