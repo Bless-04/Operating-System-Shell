383d8d032e9d68107b7248398b1b025f166a080a
Blessing <babumere04@gmail.com>
2025-05-16 07:39:04 -0400

    Update touch.hpp

--- src/Unix/touch.hpp
+++ src/Unix/touch.hpp
@@ -3,11 +3,11 @@
 void Shell::Create_Empty_Files(const vector<string> &files)
 {
     for (const string& file : files){
-        int f = open(file.c_str(), O_CREAT | O_WRONLY, 0644);
+        int f = syscall(SYS_open, file.c_str(), O_CREAT | O_WRONLY, 0644);
         if (f == -1)
             perror(("touch failed for " + file).c_str());
         else 
-            close(f);
+            syscall(SYS_close, f);
         
     }
 }
