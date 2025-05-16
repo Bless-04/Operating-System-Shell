e1890004765cde55dc7cadc03bfa7c6fc8de43b2
Blessing <babumere04@gmail.com>
2025-05-16 07:45:04 -0400

    Update dir.hpp

--- src/Unix/dir.hpp
+++ src/Unix/dir.hpp
@@ -2,23 +2,40 @@
 
 void Shell::List_Directory(const string &path)
 {
-    
-    DIR *dir = path.empty() ? opendir(".") : opendir(path.c_str());
-    if (!dir)
+    const char* dir = path.empty() ? "." : path.c_str();
+    int fd = syscall(SYS_open, dir, O_RDONLY);
+    if (fd == -1)
     {
         perror("Failed to open directory");
         return;
     }
 
-    struct dirent *data;
-    while ((data = readdir(dir)) != nullptr)
-    {
-        cout << ((data->d_type == DT_DIR) ? "<DIR>\t" : "\t");
+    /**
+        @brief Directory data
+        @note the names of the properties cant be changed or bad things happen
+    */
+    struct data {
+        unsigned long d_ino;
+        off_t d_off;
+        unsigned short d_reclen;
+        char d_name[];
+    };
 
-        //cout << data << "\tbytes";
-        cout << data->d_name << endl;
-        
+    char buffer[this->BUFFER_SIZE];
+    int nread;
+    
+    while ((nread = syscall(SYS_getdents, fd, buffer, sizeof(buffer))) > 0)
+    {
+        for (char *d = buffer; d < buffer + nread;) {
+            struct data *de = (struct data *)d;
+            cout << ((de->d_ino != 0) ? "<DIR>\t" : "\t");
+            cout << de->d_name << endl;
+            d += de->d_reclen;
+        }
     }
-    cout << endl;
-    closedir(dir);
+    
+    if (nread == -1)
+        perror("Failed to read directory");
+
+    syscall(SYS_close, fd);
 }
